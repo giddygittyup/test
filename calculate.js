0c2daa76be06781fd9354f5ec97da8432a332179
giddygittyup <sven2000@gmail.com>
2016-06-26 17:30:31 +0200

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,35 @@
+    /*  ----------------------------------------------------------------------------------
+        calculateEvaluate
+        ---------------------------------------------------------------------------------- */
+
+        function calculateEvaluate(str)
+        {
+            // evaluate expression
+
+            // str | string | calculator display
+
+            var ret = str.replace(/^\s+/, "").replace(/\s+$/, ""); // strip spaces
+
+            if (ret === "") { return ret; }
+
+            try
+            {
+                ret = eval(str); // preferrably eval is not used, use e.g. shunting yard algorithm instead
+            }
+            catch (err)
+            {
+                ret = "syntax error";
+
+                console.log(err);
+            }
+
+            return ret;
+        }
+
+
+
+
+
+
+
+
---
+++ userinput.js
@@ -0,0 +1,94 @@
+    /*  ----------------------------------------------------------------------------------
+        userInputKeyBoard
+        ---------------------------------------------------------------------------------- */
+
+        function userInputKeyBoard(evt)
+        {
+            // user input keyboard
+
+            // evt | object | event
+
+        //  console.log(evt); // uncomment this line and press F12 in Chrome or IE to see dump, click triangle to expand
+
+            if (evt.target.nodeName.toLowerCase() === "input") // always use three equal signs
+            {
+                var typ = evt.type;  // "keydown", "keypress", "input", "keyup"
+                var whi = evt.which; //
+
+                switch (evt.target.type.toLowerCase())
+                {
+                    case "text":
+                    {
+                    //  console.log("evtKeyBoard text", typ, whi);
+                        break; // always use break
+                    }
+
+                    case "button":
+                    {
+                    //  console.log("evtKeyBoard button", typ, whi);
+                        break; // always use break
+                    }
+                }
+            }
+        }
+
+    /*  ----------------------------------------------------------------------------------
+        userInputMouse
+        ---------------------------------------------------------------------------------- */
+
+        function userInputMouse(evt)
+        {
+            // user input mouse
+
+            // evt | object | event
+
+        //  console.log(evt); // uncomment this line and press F12 in Chrome or IE to see dump, click triangle to expand
+
+            var but = "";       // "left", "middle", "right"
+            var typ = evt.type; // "mousedown", "mousemove", "mouseup"
+
+            switch (evt.button)
+            {
+                case 0: { but = "left"  ; break; } // always use break
+                case 1: { but = "middle"; break; } // always use break
+                case 2: { but = "right" ; break; } // always use break
+            }
+
+            if (evt.target.nodeName.toLowerCase() === "input") // always use three equal signs
+            {
+                switch (evt.target.type.toLowerCase())
+                {
+                    case "text":
+                    {
+                    //  console.log("evtMouse text", but, typ);
+                        break; // always use break
+                    }
+
+                    case "button":
+                    {
+                        if ((typ === "mouseup") && (but === "left"))
+                        {
+                            var dis = document.getElementById("calculator display");            // HTML element | calculator display
+                            var key = evt.target.value.replace(/^\s+/, "").replace(/\s+$/, ""); // string       | calculator key (spaces stripped)
+
+                            switch (key)
+                            {
+                                case "x": { dis.value = dis.value + "*";              break; } // always use break
+                                case "=": { dis.value = calculateEvaluate(dis.value); break; } // always use break
+                                case "C": { dis.value = "";                           break; } // always use break
+                                default : { dis.value = dis.value + key;              break; } // always use break (superfluous in this case, but we still use it)
+                            }
+                        }
+
+                    //  console.log("evtMouse button", but, typ);
+                        break; // always use break
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
